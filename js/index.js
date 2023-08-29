957ac1269624a94697136810cb96a3de01e41051
jan438 <jan438@users.noreply.github.com>
2023-08-29 13:59:29 +0200

    Removed some code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-D7Animator.baseUrl = './assets/images/';
-var animators = [new D7Animator("die0", null, "dice"), new D7Animator("die1", null, "dice"), new D7Animator("die2", null, "dice"), new D7Animator("die3", null, "dice"), new D7Animator("die4", null, "dice")];
-var yahtzee = new D7AnimGroup('dice', animators, true);
 
 var YAHTZEE = {};
 
@@ -9,18 +6,4 @@ YAHTZEE.info = {result_string: "", results_id: "__results"};
 YAHTZEE.callback = function(total, info, results) {
 }
 
-callback = function() {
-
-	var dices = [yahtzee.results[0],yahtzee.results[1],yahtzee.results[2],yahtzee.results[3],yahtzee.results[4]];
-
-}
-
 D6.dice(5, YAHTZEE.callback, YAHTZEE.info);
-
-function roll() {
-	D7AnimGroup.get('dice').clear();
-	D7AnimGroup.get('dice').setCallback(callback, null)
-	D7AnimGroup.get('dice').start(yahtzee.results);
-}
-
-window.onload = roll;
