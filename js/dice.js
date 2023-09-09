2dbf75bd74d1b5ba0d7ab4176563b5eee687762f
jan438 <jan438@users.noreply.github.com>
2023-09-09 10:00:06 +0200

    Small dices initalised with blank achtergrond

--- js/dice.js
+++ js/dice.js
@@ -11,6 +11,12 @@ $(document).ready(function() {
 	var animationSpeed = dice0.css('transition-duration').split(',')[0].replace(/[^-\d\.]/g, '') * 1000;
 
 	$("#diceform").hide();
+	$("#dice1").css("background-image", "url('./assets/images/blank.gif')");
+	$("#dice2").css("background-image", "url('./assets/images/blank.gif')");
+	$("#dice3").css("background-image", "url('./assets/images/blank.gif')");
+	$("#dice4").css("background-image", "url('./assets/images/blank.gif')");
+	$("#dice5").css("background-image", "url('./assets/images/blank.gif')");
+
 	document.getElementById("dicebutton").click();
 	setTimeout(function() {
 		$('#dicebutton').trigger('click');
