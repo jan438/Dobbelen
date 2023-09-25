98585f0f0da6f3caad3d858aee4745b7c9a098a6
jan438 <jan438@users.noreply.github.com>
2023-09-25 14:22:49 +0200

    Var airesults defined

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 var aiturncount = 0;
 var airollcount = 0;
 var YAHTZEE = {};
+var airesults = [];
 YAHTZEE.info = {};
 YAHTZEE.callback = function(total, info, results) {
-	console.log("Callback total: " + total + " results: " + results + " aiturncount: " + aiturncount + " airollcount: " + airollcount);
+	airesults = results;
+//	console.log("Callback total: " + total + " results: " + results + " aiturncount: " + aiturncount + " airollcount: " + airollcount);
 }
 YAHTZEE.findCombinations = function(results) {
 	console.log("Findcombination: " + results);
