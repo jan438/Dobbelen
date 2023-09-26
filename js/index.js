c18ead9a7608ed3b19954a46bf3a27f2efadce38
jan438 <jan438@users.noreply.github.com>
2023-09-26 10:33:48 +0200

    Sorted functie added

--- js/index.js
+++ js/index.js
@@ -6,8 +6,22 @@ YAHTZEE.info = {};
 YAHTZEE.callback = function(total, info, results) {
 	airesults = results;
 //	console.log("Callback total: " + total + " results: " + results + " aiturncount: " + aiturncount + " airollcount: " + airollcount);
+	YAHTZEE.sort_results(airesults);
 }
 YAHTZEE.findCombinations = function(results) {
 	console.log("Findcombination: " + results);
+}
+YAHTZEE.sort_results = function(results) {
+  var sorted = results;
+  var temp;
+  for (i = sorted.length - 1; i >= 1; --i) {
+    for (j = 0; j < i; ++j) {
+	if ( sorted[j] > sorted[j + 1] ) {
+           temp = sorted[j];
+           sorted[j] = sorted[j + 1];
+           sorted[j + 1] = temp;
+       }
+    }
+  }
 }
 D6.dice(5, YAHTZEE.callback, YAHTZEE.info);
