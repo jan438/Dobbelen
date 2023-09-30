4deef26516ef6155fc936399d6e99082ff4c09a1
jan438 <jan438@users.noreply.github.com>
2023-09-30 14:13:19 +0200

    Human dices initialized simplified

--- js/dice.js
+++ js/dice.js
@@ -13,41 +13,17 @@ $(document).ready(function() {
 	var val;
 	var hmresults = [0,0,0,0,0];
 	var hmdicetabel = [1,6,4,3,2,5];
-	var random = Math.floor((Math.random() * 6) + 1);
-	if (random == 1) { rollDice(dice0, 'front'); }
-	else if (random == 2) { rollDice(dice0, 'back'); }
-	else if (random == 3) { rollDice(dice0, 'right'); }
-	else if (random == 4) { rollDice(dice0,'left'); }
-	else if (random == 5) { rollDice(dice0, 'top'); }
-	else if (random == 6) { rollDice(dice0, 'bottom'); }
-	random = Math.floor((Math.random() * 6) + 1);
-	if (random == 1) { rollDice(dice1, 'front'); }
-	else if (random == 2) { rollDice(dice1, 'back'); }
-	else if (random == 3) { rollDice(dice1, 'right'); }
-	else if (random == 4) { rollDice(dice1,'left'); }
-	else if (random == 5) { rollDice(dice1, 'top'); }
-	else if (random == 6) { rollDice(dice1, 'bottom'); }
-	random = Math.floor((Math.random() * 6) + 1);
-	if (random == 1) { rollDice(dice2, 'front'); }
-	else if (random == 2) { rollDice(dice2, 'back'); }
-	else if (random == 3) { rollDice(dice2, 'right'); }
-	else if (random == 4) { rollDice(dice2,'left'); }
-	else if (random == 5) { rollDice(dice2, 'top'); }
-	else if (random == 6) { rollDice(dice2, 'bottom'); }
-	random = Math.floor((Math.random() * 6) + 1);
-	if (random == 1) { rollDice(dice3, 'front'); }
-	else if (random == 2) { rollDice(dice3, 'back'); }
-	else if (random == 3) { rollDice(dice3, 'right'); }
-	else if (random == 4) { rollDice(dice3,'left'); }
-	else if (random == 5) { rollDice(dice3, 'top'); }
-	else if (random == 6) { rollDice(dice3, 'bottom'); }
-	random = Math.floor((Math.random() * 6) + 1);
-	if (random == 1) { rollDice(dice4, 'front'); }
-	else if (random == 2) { rollDice(dice4, 'back'); }
-	else if (random == 3) { rollDice(dice4, 'right'); }
-	else if (random == 4) { rollDice(dice4,'left'); }
-	else if (random == 5) { rollDice(dice4, 'top'); }
-	else if (random == 6) { rollDice(dice4, 'bottom'); }
+	var dicesides = ['front','back','right','left','top','bottom'];
+	var random = Math.floor((Math.random() * 6));
+	rollDice(dice0, dicesides[random]);
+	random = Math.floor((Math.random() * 6));
+	rollDice(dice1, dicesides[random]);
+	random = Math.floor((Math.random() * 6));
+	rollDice(dice2, dicesides[random]);
+	random = Math.floor((Math.random() * 6));
+	rollDice(dice3, dicesides[random]);
+	random = Math.floor((Math.random() * 6));
+	rollDice(dice4, dicesides[random]);
 
 	$("#diceform").hide();
 	$("#dice1").css("background-image", "url('./assets/images/green.png')");
